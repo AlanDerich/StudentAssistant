62a29c39ea688ff8e0331198a54759295fb9aefe
eugeneawilly98 <eugenemrlens@gmail.com>
2020-03-01 08:43:31 +0300

    Conected Project to Firebase and added the firebase adatabase dependencies

--- app/src/main/java/com/eujoh/student_assistant/contacts/LecturersContActivity.kt
+++ app/src/main/java/com/eujoh/student_assistant/contacts/LecturersContActivity.kt
@@ -2,6 +2,9 @@ package com.eujoh.student_assistant.contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -14,13 +17,25 @@ class LecturersContActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lecturers_cont)
 
+        var faculties = arrayOf("School of Science", "School of Engineering", "School of Education", "School of Business", "School of Agriculture")
+        spinner_faculty.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,faculties)
+        spinner_faculty.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+
+        }
+
         val adapter = MyViewPagerAdapter(supportFragmentManager)
         adapter.addFragment(FragmentFullTimeLec(), "FULL-TIME")
         adapter.addFragment(FragmentPartTimeLec(), "PART-TIME")
         adapter.addFragment(FragmentFavLec(), "")
         lecturers_viewPager.adapter = adapter
         lecturers_tabLayout.setupWithViewPager(lecturers_viewPager)
-
         lecturers_tabLayout.getTabAt(2)!!.setIcon(R.drawable.ic_fav)
     }
 
